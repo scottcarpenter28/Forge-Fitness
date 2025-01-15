039458284c24f3626fb329f8598924d3ffa9e2e7
Scott Carpenter <scottcarpenter28@gmail.com>
2025-01-15 18:49:13 -0500

    Start building page to run routines

--- src/project/application/views/run_routine.py
+++ src/project/application/views/run_routine.py
@@ -1,7 +1,20 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import Http404
+from django.shortcuts import render, redirect
+
+from application.models.exercise_routine import ExerciseRoutine
 
 
 @login_required
 def run_routine(request, routine_id: str):
-    raise Http404("Running routines is not yet implemented.")
+    if not routine_id:
+        messages.error(request, "Routine ID is required.")
+        return redirect("/")
+
+    found_routine = ExerciseRoutine.objects.filter(
+        uuid=routine_id, creator=request.user
+    ).first()
+    if not found_routine:
+        messages.error(request, "Routine not found")
+        return redirect("/")
+    return render(request, "application/run_routine.html")
